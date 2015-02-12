bfd6450b8fc1c6d31b4c2b9a53480b0cb7773cfc
yshay <yshayy@gmail.com>
2015-02-12 15:46:11 +0200

    added status screen

--- main.js
+++ main.js
@@ -42,17 +42,19 @@ var loginRoute = function()
         } else {
           users.child(authData.uid).set({
             provider: authData.provider,
+            authData: authData,
             firstName: authData.google.cachedUserProfile.given_name,
             lastName: authData.google.cachedUserProfile.family_name,
-            pictuure: authData.google.cachedUserProfile.picture
+            picture: authData.google.cachedUserProfile.picture,
+            id: authData.uid
           });
-          app.router.setRoute("home");
+          app.router.setRoute("/");
         }
       })
     }});
 }
 
-var reviewStatusRoute = function(id)
+var reviewStatusRoute = function(group,id)
 {
     console.log(id);
     ractive = new Ractive({
@@ -61,8 +63,31 @@ var reviewStatusRoute = function(id)
 
       template: '#ReviewStatusTemplate',
 
-      data: {}
+      data: {
+        users:[],
+      }
     });
+    
+    reviews.child(group)
+            .child(id)
+           .child("users")
+           .on("child_added", function(snapshot)
+            {
+                console.log("new child added");
+                ractive.data.users.push(snapshot.val());
+            });
+    
+    reviews.child(group)
+            .child(id)
+           .child("users")
+           .on("child_changed", function(snapshot)
+            {
+                var updatedUser = snapshot.val();
+                var targetUser = ractive.data.users.filter(function(u){ return u.id === updatedUser.id;})[0];
+                $.extend(targetUser, updatedUser);
+                ractive.update("users");
+            });
+    
 }
 
 var requestReviewRoute = function()
@@ -90,32 +115,76 @@ var requestReviewRoute = function()
                 status: 'pending',
                 date: new Date(),
                 description: ractive.data.description,
-                reviewee: db.getAuth().uid,                
+                reviewee: db.getAuth().uid, 
+            });
+            users.on("value",function(data){
+                var allUsers = data.val();
+                for (var userId in allUsers)
+                {
+                    if (userId == db.getAuth().uid) continue;
+                    var user = allUsers[userId];
+                    review.child("users").push(
+                        {
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            id: userId,
+                            picutre: user.picture,
+                            status: "pending"
+                        });
+                }
             });
             
-            app.router.setRoute('ReviewStatus/'+review.name());
+            
+            app.router.setRoute('ReviewStatus/'+ ractive.data.group + "/" +review.name());
             
         } 
     });
 }
 
-var routes = {
-        '/': function()
+function respond(id)
+{
+    
+}
+
+var auth = function(fn)
+{
+    return function()
+    {
+        if (db.getAuth())
         {
-          console.log('/');
-          if (db.getAuth())
-          {
-             app.router.setRoute("home");
-          }
-          else
-          {
-            app.router.setRoute("login");
-          }
-        },
-        '/home': homeRoute,
+            fn.apply(this,arguments);
+        }
+        else
+        {
+            app.router.setRoute("/login");
+        }
+    }
+}
+
+var routes = {
+        '/selectgroup': auth(function()
+        {
+            var group = "";
+            if (confirm("are you developers"))
+            {
+                group = "Dev";
+            }
+            else if (confirm("are you ux"))
+            {
+                group = "UX";
+            }
+            else
+            {
+                group = "Product";
+            }
+            
+            users.set()
+        }),
+        '/': auth(homeRoute),
         '/login': loginRoute,
-        '/request': requestReviewRoute,
-        '/ReviewStatus/:id': reviewStatusRoute
+        '/request': auth(requestReviewRoute),
+        '/ReviewStatus/:group/:id': auth(reviewStatusRoute),
+        '/Respond/:id':auth(respond)
       };
 
 
